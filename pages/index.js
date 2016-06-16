7edafb05fbf7efd1ce960220cec5a51037bc3e72
Jon Gold <hello@designedbygold.com>
2016-06-16 13:14:14 +0100

    Add Zach's letter

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,9 @@ export default class Index extends React.Component {
           <h1 className="tk-p22-underground ttu red lh-title">{config.hero.title}</h1>
           <h2 className="tk-p22-underground lh-title">{ config.hero.subtitle }</h2>
         </section>
+        <section className="pa3">
+          <Paragraphs className="f5 f4-ns measure-wide lh-copy tk-franklin-gothic-urw">{ config.letter.text }</Paragraphs>
+        </section>
         <div className="cf">
           <PersonGrid title="Goodies" text={ config.goodies.text } people={ config.goodies.people } />
           <PersonGrid title="Baddies" text={ config.baddies.text } people={ config.baddies.people } />
@@ -41,49 +44,6 @@ export default class Index extends React.Component {
           <div className="bg-red white tk-p22-underground dib pv3 ph4 br2 f4 ttu tracked">Testing</div>
         </section>
       </div>
-        {/* <div> */}
-        {/*   <h1> */}
-        {/*     Hi people */}
-        {/*   </h1> */}
-        {/*   <p>Welcome to your new Gatsby site</p> */}
-        {/*   <h2>Below are some pages showing different capabilities built-in to Gatsby</h2> */}
-        {/*   <h3>Supported file types</h3> */}
-        {/*   <ul> */}
-        {/*     <li> */}
-        {/*       <Link to={prefixLink('/markdown/')}>Markdown</Link> */}
-        {/*     </li> */}
-        {/*     <li> */}
-        {/*       <Link to={prefixLink('/react/')}>JSX (React components)</Link> */}
-        {/*     </li> */}
-        {/*     <li> */}
-        {/*       <Link to={prefixLink('/coffee-react/')}>CJSX (Coffeescript React components)</Link> */}
-        {/*     </li> */}
-        {/*     <li> */}
-        {/*       <Link to={prefixLink('/html/')}>HTML</Link> */}
-        {/*     </li> */}
-        {/*     <li> */}
-        {/*       <Link to={prefixLink('/json/')}>JSON</Link> */}
-        {/*     </li> */}
-        {/*     <li> */}
-        {/*       <Link to={prefixLink('/yaml/')}>YAML</Link> */}
-        {/*     </li> */}
-        {/*     <li> */}
-        {/*       <Link to={prefixLink('/toml/')}>TOML</Link> */}
-        {/*     </li> */}
-        {/*   </ul> */}
-        {/*   <h3>Supported CSS processors</h3> */}
-        {/*   <ul> */}
-        {/*     <li> */}
-        {/*       <Link to={prefixLink('/postcss/')}>PostCSS</Link> */}
-        {/*     </li> */}
-        {/*     <li> */}
-        {/*       <Link to={prefixLink('/sass/')}>Sass</Link> */}
-        {/*     </li> */}
-        {/*     <li> */}
-        {/*       <Link to={prefixLink('/less/')}>Less</Link> */}
-        {/*     </li> */}
-        {/*   </ul> */}
-        {/* </div> */}
       </DocumentTitle>
     )
   }
